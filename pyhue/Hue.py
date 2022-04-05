c87ac484dbabf7f33a771e87baa99537bf618975
Jakob Kampichler <jakob.kampichler@gmail.com>
2022-04-05 14:30:46 +0200

    Merged into one file. Will probably fail but yk, testing

--- pyhue/Hue.py
+++ pyhue/Hue.py
@@ -1,10 +1,44 @@
 from operator import contains
 import requests as reqs
+from requests import get
 import urllib3 as urll3
 import os
 import pickle
+from zeroconf import ServiceBrowser, Zeroconf
+from time import sleep
+import socket
 
-from Discovery import BrowseMdns, BrowseEndpoint
+class __mdns_listener:
+    def __init__(self, add):
+        self.__add = add
+
+    def remove_service(self, zeroconf, type, name):
+        "empty"
+
+    def update_service(self, zeroconf, type, name):
+        "empty"
+
+    def add_service(self, zeroconf, type, name):
+        serviceInfo = zeroconf.get_service_info(type, name)
+        ip = "err"
+        if serviceInfo:
+            ip = socket.inet_ntoa(serviceInfo.addresses[0])
+
+        self.__add(name=name, ip=ip)
+
+
+def BrowseEndpoint(addF):
+    res = get("https://discovery.meethue.com/")
+    for bridge in res.json():
+        addF(name=bridge["name"], ip=bridge["internalipaddress"])
+
+
+def BrowseMdns(target, addF):
+    zeroConf = Zeroconf()
+    listener = __mdns_listener(add=addF)
+    browser = ServiceBrowser(zeroConf, target, listener)
+    sleep(3)
+    zeroConf.close()
 
 
 class Hue:
