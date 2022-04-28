96b920de3d45759baf566a20fb1f156457a2904c
theJakobcraft <39417522+jkampich1411@users.noreply.github.com>
2022-04-28 21:23:29 +0200

    okay again a new version what is this

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import pathlib
 HERE = pathlib.Path(__file__).parent
 setup(
     name="py3-PyHue",
-    version="0.1a1",
+    version="0.1a3",
     author="Jakob K",
-    description="Python3 library for Philips Hue",
+    description="Python3 Module for controlling Philips Hue lights",
     long_description=(HERE / "README.md").read_text(),
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=[".cached*"]),
