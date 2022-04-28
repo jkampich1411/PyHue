474442bfdb254817f736b53d69ebab06dd087092
theJakobcraft <39417522+jkampich1411@users.noreply.github.com>
2022-04-28 20:31:51 +0200

    updated

--- setup.py
+++ setup.py
@@ -2,25 +2,20 @@ from setuptools import setup, find_packages
 import pathlib
 
 HERE = pathlib.Path(__file__).parent
-
-README = (HERE / "README.md").read_text()
-LICENSE = (HERE / "LICENSE").read_text()
-VERSION = (HERE / "VERSION").read_text()
-
 setup(
-    name="PyHue",
-    version=VERSION,
+    name="py3-PyHue",
+    version="0.1a0",
     author="Jakob K",
     description="Python3 library for Philips Hue",
-    long_description=README,
+    long_description=(HERE / "README.md").read_text(),
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=[".cached*"]),
     url="https://github.com/jkampich1411/PyHue",
-
     requires=["zeroconf", "requests"],
     install_requires=["zeroconf", "requests"],
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
-    license="MIT",
+    license="https://jkdev.mit-license.org/",
     author_email="me@jkdev.run",
 )
