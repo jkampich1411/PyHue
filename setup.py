d58df024b5039d7b5cb3136770fdaa24a1cbbf18
theJakobcraft <39417522+jkampich1411@users.noreply.github.com>
2022-04-28 16:06:51 +0200

    lul

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ HERE = pathlib.Path(__file__).parent
 
 README = (HERE / "README.md").read_text()
 LICENSE = (HERE / "LICENSE").read_text()
+VERSION = (HERE / "VERSION").read_text()
 
 setup(
-    name="pyhue",
-    version="0.0.1",
+    name="PyHue",
+    version=VERSION,
     author="Jakob K",
-    description="Python library for Philips Hue",
+    description="Python3 library for Philips Hue",
     long_description=README,
     packages=find_packages(exclude=[".cached*"]),
-    url="https://github.com/jkampich1411/pyhue",
+    url="https://github.com/jkampich1411/PyHue",
 
     requires=["zeroconf", "requests"],
     install_requires=["zeroconf", "requests"],
